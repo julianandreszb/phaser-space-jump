29a5be4903addfc1f697cf315d12231208931f23
jzapata <jzapata@lifefile.net>
2023-06-20 21:08:01 -0500

    feat: game base

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Phaser from './lib/phaser.js';
+import Game from "./scenes/Game.js";
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Game
+});
